1346e09759a7a643f58b3e1f6e3a3e5d1b488c6d
Lee Robinson <lrobinson2011@gmail.com>
2019-01-06 13:32:45 -0600

    Update about page to include travel map.

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 import {heading} from '../styles/mixins';
 import {spacing} from '../styles/vars';
+import A from '../components/elements/a';
 import Footer from '../components/footer';
 import H3 from '../components/elements/h3';
 import Main from '../components/main';
@@ -25,11 +26,27 @@ const Index = () => (
         <Main>
             <AboutTitle>{'About Me'}</AboutTitle>
             <p>
-                {`Born and raised in Iowa, I'm currently living in Des Moines and working as a Software Engineer at Hy-Vee.`}
+                {`Born and raised in Iowa, I'm currently living in Des Moines and working as a Software Engineer at `}
+                <A href="https://www.hy-vee.com">{'Hy-Vee.'}</A>
             </p>
-            <p>{'I enjoy all things technology, videography, and music.'}</p>
+            <p />
+            <p>
+                {'I enjoy all things technology, videography, and music. '}
+                {'For a closer look at the professional work I’ve done, check out my '}
+                <A href="/static/LeeRobinson.pdf">{'resume.'}</A>
+                {' If you’re interested in reaching out to me, feel free to '}
+                <A href="mailto:lrobinson2011@gmail.com">{'email me.'}</A>
+            </p>
             <H3>{'Contact'}</H3>
             <Social />
+            <H3>{'Travel Map'}</H3>
+            <p>{' Check out the map below to see every place I’ve traveled to ✈️ '}</p>
+            <iframe
+                height="280"
+                src="https://www.google.com/maps/d/embed?mid=1QOGi-u8d4vwoQ4vC4zQjKxrSfsDIQdOK&hl=en"
+                title="Lee's Travel Map"
+                width="100%"
+            />
         </Main>
         <Footer />
     </Page>
