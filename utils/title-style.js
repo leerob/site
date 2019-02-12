7da1922f983ca8ded1f284f210dff8c1fc9b2dce
Lee Robinson <lrobinson2011@gmail.com>
2019-02-12 15:00:12 -0600

    Add Hy-Vee articles and update blog post link.

--- utils/title-style.js
+++ utils/title-style.js
@@ -2,5 +2,5 @@ import title from 'title';
 
 export default (text) =>
     title(text, {
-        special: ['CSS', 'iPhone', 'USB', 'HDD', '1Password', 'QC35', 'VSCode', 'iTerm', 'About', 'MDX']
+        special: ['CSS', 'iPhone', 'USB', 'HDD', '1Password', 'QC35', 'VSCode', 'iTerm', 'About', 'MDX', 'UI']
     });
