c73a6f6c54ea6770faf79791c2487b39be464ed8
Lee Robinson <lrobinson2011@gmail.com>
2020-03-22 20:51:28 -0500

    Remove next-optimized-images.

--- next.config.js
+++ next.config.js
@@ -1,41 +1,29 @@
 const readingTime = require('reading-time');
 const mdxPrism = require('mdx-prism');
-const withPlugins = require('next-compose-plugins');
-const withOptimizedImages = require('next-optimized-images');
 const withMdxEnhanced = require('next-mdx-enhanced');
-const remarkPlugins = [
-  require('remark-autolink-headings'),
-  require('remark-slug'),
-  require('remark-code-titles'),
-  require('remark-capitalize')
-];
 
-module.exports = withPlugins(
-  [
-    withMdxEnhanced({
-      layoutPath: 'layouts',
-      defaultLayout: true,
-      remarkPlugins,
-      rehypePlugins: [mdxPrism],
-      extendFrontMatter: {
-        process: (mdxContent) => ({
-          wordCount: mdxContent.split(/\s+/gu).length,
-          readingTime: readingTime(mdxContent)
-        })
-      }
-    })({
-      pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
-    }),
-    withOptimizedImages
+module.exports = withMdxEnhanced({
+  layoutPath: 'layouts',
+  defaultLayout: true,
+  remarkPlugins: [
+    require('remark-autolink-headings'),
+    require('remark-slug'),
+    require('remark-code-titles'),
+    require('remark-capitalize')
   ],
-  {
-    // Next.js config
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        require('./scripts/generate-sitemap');
-      }
-
-      return config;
-    }
+  rehypePlugins: [mdxPrism],
+  extendFrontMatter: {
+    process: (mdxContent) => ({
+      wordCount: mdxContent.split(/\s+/gu).length,
+      readingTime: readingTime(mdxContent)
+    })
   }
-);
+})({
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+
+    return config;
+  }
+});
