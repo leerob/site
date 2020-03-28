8a06e4d7ab2f732ff22bbac9d20a502ffb652052
Lee Robinson <lrobinson2011@gmail.com>
2020-03-28 11:24:38 -0500

    Better SEO and OG images. (#160)

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NextSeo } from 'next-seo';
 import {
   useColorMode,
   Heading,
@@ -11,99 +12,141 @@ import {
 
 import Container from '../components/Container';
 
+const url = 'https://leerob.io/about';
+const title = 'About Me – Lee Robinson';
+
 const About = () => {
   const { colorMode } = useColorMode();
   const secondaryTextColor = {
     light: 'gray.700',
     dark: 'gray.400'
   };
+  const linkColor = {
+    light: 'hsl(208, 99%, 44%)',
+    dark: 'hsl(208, 95%, 68%)'
+  };
 
   return (
-    <Container>
-      <Stack
-        as="main"
-        spacing={8}
-        justifyContent="center"
-        alignItems="flex-start"
-        m="0 auto 4rem auto"
-        maxWidth="700px"
-      >
-        <Flex
-          flexDirection="column"
-          justifyContent="flex-start"
+    <>
+      <NextSeo
+        title={title}
+        canonical={url}
+        openGraph={{
+          url,
+          title
+        }}
+      />
+      <Container>
+        <Stack
+          as="main"
+          spacing={8}
+          justifyContent="center"
           alignItems="flex-start"
+          m="0 auto 4rem auto"
           maxWidth="700px"
         >
-          <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
-            About Me
-          </Heading>
-          <Text color={secondaryTextColor[colorMode]} mb={4}>
-            Lee Robinson is a developer, writer, and UI/UX enthusiast. He works
-            at Hy-Vee, a $10 billion dollar retailer in the Midwest. His primary
-            focus is developing Aisles Online, Hy-Vee's e-commerce grocery
-            shopping platform, which serves millions of customers every year.
-          </Text>
-          <Text color={secondaryTextColor[colorMode]} mb={4}>
-            Lee has spoken across the country at conferences and meet-ups about
-            front-end development, design, and recruiting. He also runs his own
-            blog, attracting thousands of readers every month.
-          </Text>
-          <Text color={secondaryTextColor[colorMode]} mb={4}>
-            Prior to Hy-Vee, Lee worked as an engineer at Workiva (NYSE:WK) - a
-            cloud provider of connected data, reporting, and compliance
-            solutions. Lee enjoys all things technology, music, and
-            photography/videography.
-          </Text>
-          <Heading letterSpacing="tight" mt={8} mb={4} as="h2" size="xl">
-            Conference Talks
-          </Heading>
-          <Heading size="md" as="h3" mb={2} fontWeight="medium">
-            <Link
-              display="flex"
-              href="/building-component-libraries-with-a-monorepo.pdf"
-              isExternal
-            >
-              <Flex align="center">
-                Building Component Libraries with a Monorepo
-                <Icon name="external-link" mx={2} size="16px" />
-              </Flex>
-            </Link>
-          </Heading>
-          <Text color={secondaryTextColor[colorMode]} mb={8}>
-            Learn why your organization needs a component library and discover
-            the best practices for building, scaling, and adopting it across all
-            platforms. We'll be using industry-standard technology (React,
-            JavaScript, Storybook) alongside cutting-edge solutions (CSS-in-JS,
-            Monorepo).
-          </Text>
-          <Heading size="md" as="h3" mb={2} fontWeight="medium">
-            <Link
-              display="flex"
-              href="/recruiting-engineers-talent42-lee-robinson.pdf"
-              isExternal
-            >
-              <Flex align="center">
-                Recruiting Engineers (From An Engineer's Perspective)
-                <Icon name="external-link" mx={2} size="16px" />
-              </Flex>
-            </Link>
-          </Heading>
-          <Text color={secondaryTextColor[colorMode]} mb={8}>
-            Hiring talent is becoming increasingly difficult with low
-            unemployment rates and the tech industry booming. What you can do to
-            stick out? Learn from an engineer who's been involved on both sides
-            - both as a candidate and with hiring - on what candidates really
-            want out of a position.
-          </Text>
-          <iframe
-            height="280"
-            src="https://www.google.com/maps/d/embed?mid=1QOGi-u8d4vwoQ4vC4zQjKxrSfsDIQdOK&hl=en"
-            title="Lee's Travel Map"
-            width="100%"
-          />
-        </Flex>
-      </Stack>
-    </Container>
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            maxWidth="700px"
+          >
+            <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
+              About Me
+            </Heading>
+            <Text color={secondaryTextColor[colorMode]} mb={4}>
+              Hey, I’m Lee. I live in Des Moines, IA and I’m a Senior Software
+              Engineer at&nbsp;
+              <Link
+                href="https://innovate.hy-vee.com"
+                title="Hy-Vee"
+                isExternal
+                color={linkColor[colorMode]}
+              >
+                Hy-Vee
+              </Link>
+              , a $10B grocery tech company in the Midwest. As a tech lead, my
+              primary focus is developing Aisles Online, Hy-Vee’s e-commerce
+              grocery shopping platform – which serves millions of customers
+              every year. I also help maintain our internal component library.
+            </Text>
+            <Text color={secondaryTextColor[colorMode]} mb={4}>
+              At the beginning of 2020, I launched my most ambitious project – a
+              50+ lesson video course on React and Next.js. It’s called&nbsp;
+              <Link
+                href="https://masteringnextjs.com"
+                title="Mastering Next.js"
+                isExternal
+                color={linkColor[colorMode]}
+              >
+                Mastering Next.js
+              </Link>
+              , and it’s been my most successful solo-endeavor so far.
+            </Text>
+            <Text color={secondaryTextColor[colorMode]} mb={4}>
+              I’ve spoken across the country at conferences and meet-ups about
+              front-end development, design, and recruiting. I also actively
+              contribute to my blog, which attracts thousands of readers every
+              month. I mostly write about development, tech careers, and my
+              personal life.
+            </Text>
+            <Text color={secondaryTextColor[colorMode]} mb={4}>
+              I grew up in small-town Iowa and went to school at Iowa State,
+              graduating with a degree in Computer Engineering. I spend my free
+              time playing music, creating videos, and enjoying time with
+              friends and family
+            </Text>
+            <Heading letterSpacing="tight" mt={8} mb={4} as="h2" size="xl">
+              Conference Talks
+            </Heading>
+            <Heading size="md" as="h3" mb={2} fontWeight="medium">
+              <Link
+                display="flex"
+                href="/building-component-libraries-with-a-monorepo.pdf"
+                isExternal
+              >
+                <Flex align="center">
+                  Building Component Libraries with a Monorepo
+                  <Icon name="external-link" mx={2} size="16px" />
+                </Flex>
+              </Link>
+            </Heading>
+            <Text color={secondaryTextColor[colorMode]} mb={8}>
+              Learn why your organization needs a component library and discover
+              the best practices for building, scaling, and adopting it across
+              all platforms. We'll be using industry-standard technology (React,
+              JavaScript, Storybook) alongside cutting-edge solutions
+              (CSS-in-JS, Monorepo).
+            </Text>
+            <Heading size="md" as="h3" mb={2} fontWeight="medium">
+              <Link
+                display="flex"
+                href="/recruiting-engineers-talent42-lee-robinson.pdf"
+                isExternal
+              >
+                <Flex align="center">
+                  Recruiting Engineers (From An Engineer's Perspective)
+                  <Icon name="external-link" mx={2} size="16px" />
+                </Flex>
+              </Link>
+            </Heading>
+            <Text color={secondaryTextColor[colorMode]} mb={8}>
+              Hiring talent is becoming increasingly difficult with low
+              unemployment rates and the tech industry booming. What you can do
+              to stick out? Learn from an engineer who's been involved on both
+              sides - both as a candidate and with hiring - on what candidates
+              really want out of a position.
+            </Text>
+            <iframe
+              height="280"
+              src="https://www.google.com/maps/d/embed?mid=1QOGi-u8d4vwoQ4vC4zQjKxrSfsDIQdOK&hl=en"
+              title="Lee's Travel Map"
+              width="100%"
+            />
+          </Flex>
+        </Stack>
+      </Container>
+    </>
   );
 };
 
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NextSeo } from 'next-seo';
 import { useColorMode, Heading, Text, Flex, Stack } from '@chakra-ui/core';
 
 import Unsplash from '../components/metrics/unsplash';
@@ -6,6 +7,11 @@ import YouTube from '../components/metrics/youtube';
 import GoogleAnalytics from '../components/metrics/analytics';
 import GitHub from '../components/metrics/github';
 import Container from '../components/Container';
+
+const url = 'https://leerob.io/dashboard';
+const title = 'Dashboard – Lee Robinson';
+const description =
+  'My personal dashboard, built with Next.js API routes deployed as serverless functions.';
 
 const Dashboard = () => {
   const { colorMode } = useColorMode();
@@ -15,45 +21,65 @@ const Dashboard = () => {
   };
 
   return (
-    <Container>
-      <Stack
-        as="main"
-        spacing={8}
-        justifyContent="center"
-        alignItems="flex-start"
-        m="0 auto 4rem auto"
-        maxWidth="700px"
-      >
-        <Flex
-          flexDirection="column"
-          justifyContent="flex-start"
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          url,
+          title,
+          description,
+          images: [
+            {
+              url: 'https://leerob.io/static/images/dashboard.jpg',
+              alt: description,
+              width: 1280,
+              height: 720
+            }
+          ]
+        }}
+      />
+      <Container>
+        <Stack
+          as="main"
+          spacing={8}
+          justifyContent="center"
           alignItems="flex-start"
+          m="0 auto 4rem auto"
           maxWidth="700px"
         >
-          <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
-            Dashboard
-          </Heading>
-          <Text color={secondaryTextColor[colorMode]}>
-            This is my personal dashboard, built with Next.js API routes
-            deployed as serverless functions. I use this dashboard to track
-            various metrics across platforms like Unsplash, Google Analytics,
-            GitHub, and more.
-          </Text>
-        </Flex>
-        <Flex
-          flexDirection="column"
-          justifyContent="flex-start"
-          alignItems="flex-start"
-          maxWidth="700px"
-          mt={8}
-        >
-          <Unsplash />
-          <YouTube />
-          <GoogleAnalytics />
-          <GitHub />
-        </Flex>
-      </Stack>
-    </Container>
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            maxWidth="700px"
+          >
+            <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
+              Dashboard
+            </Heading>
+            <Text color={secondaryTextColor[colorMode]}>
+              This is my personal dashboard, built with Next.js API routes
+              deployed as serverless functions. I use this dashboard to track
+              various metrics across platforms like Unsplash, Google Analytics,
+              GitHub, and more.
+            </Text>
+          </Flex>
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            maxWidth="700px"
+            mt={8}
+          >
+            <Unsplash />
+            <YouTube />
+            <GoogleAnalytics />
+            <GitHub />
+          </Flex>
+        </Stack>
+      </Container>
+    </>
   );
 };
 
