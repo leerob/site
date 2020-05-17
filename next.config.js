16084e77cde04f2e48641aa994956b86240dc498
Lee Robinson <lrobinson2011@gmail.com>
2020-05-17 12:55:27 -0500

    RIP Preact - No fast refresh.

--- next.config.js
+++ next.config.js
@@ -27,36 +27,6 @@ module.exports = withMdxEnhanced({
       require('./scripts/generate-sitemap');
     }
 
-    /*
-     * https://github.com/developit/nextjs-preact-demo
-     * https://preactjs.com/guide/v10/differences-to-react
-     */
-    const splitChunks = config.optimization && config.optimization.splitChunks;
-
-    if (splitChunks) {
-      const cacheGroups = splitChunks.cacheGroups;
-      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/u;
-
-      if (cacheGroups.framework) {
-        cacheGroups.preact = {
-          ...cacheGroups.framework,
-          test: preactModules
-        };
-        cacheGroups.commons.name = 'framework';
-      } else {
-        cacheGroups.preact = {
-          name: 'commons',
-          chunks: 'all',
-          test: preactModules
-        };
-      }
-    }
-
-    // eslint-disable-next-line no-param-reassign
-    const aliases = config.resolve.alias || (config.resolve.alias = {});
-
-    aliases.react = aliases['react-dom'] = 'preact/compat';
-
     return config;
   }
 });
