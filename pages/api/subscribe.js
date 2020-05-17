75fba297ca5d0e29b5a0d2f4ad25760460da8ea5
Lee Robinson <lrobinson2011@gmail.com>
2020-05-17 10:33:08 -0500

    Update to Next 9.4 and try Fathom Analytics.

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -23,7 +23,7 @@ export default async (req, res) => {
 
     if (response.status >= 400) {
       return res.status(400).json({
-        error: `There was an error subscribing to the newsletter. Shoot me an email at [me@leerob.io] and I'll add you to the list.`
+        error: await response.text()
       });
     }
 
