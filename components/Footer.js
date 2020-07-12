913ef02ac2a6ee2046be9f1b3fa3ed9383bd419d
Lee Robinson <lrobinson2011@gmail.com>
2020-07-12 17:40:24 -0500

    Add Spotify now playing to footer.

--- components/Footer.js
+++ components/Footer.js
@@ -1,48 +1,53 @@
 import React from 'react';
 import { Flex, Link, IconButton } from '@chakra-ui/core';
 
+import NowPlaying from './NowPlaying';
+
 const Footer = () => (
-  <Flex justify="center" mb={4}>
-    <Link href="https://twitter.com/leeerob" title="Twitter" isExternal>
-      <IconButton
-        aria-label="Twitter"
-        icon="twitter"
-        size="lg"
-        color="gray.500"
-        variant="ghost"
-      />
-    </Link>
-    <Link href="https://github.com/leerob" title="GitHub" isExternal>
-      <IconButton
-        aria-label="GitHub"
-        icon="github"
-        size="lg"
-        color="gray.500"
-        variant="ghost"
-      />
-    </Link>
-    <Link
-      href="https://www.linkedin.com/in/leejamesrobinson/"
-      title="LinkedIn"
-      isExternal
-    >
-      <IconButton
-        aria-label="LinkedIn"
-        icon="linkedin"
-        size="lg"
-        color="gray.500"
-        variant="ghost"
-      />
-    </Link>
-    <Link href="mailto:me@leerob.io" title="Email" isExternal>
-      <IconButton
-        aria-label="Email"
-        icon="mail"
-        size="lg"
-        color="gray.500"
-        variant="ghost"
-      />
-    </Link>
+  <Flex align="center" mb={4} direction="column">
+    <NowPlaying />
+    <div>
+      <Link href="https://twitter.com/leeerob" title="Twitter" isExternal>
+        <IconButton
+          aria-label="Twitter"
+          icon="twitter"
+          size="lg"
+          color="gray.500"
+          variant="ghost"
+        />
+      </Link>
+      <Link href="https://github.com/leerob" title="GitHub" isExternal>
+        <IconButton
+          aria-label="GitHub"
+          icon="github"
+          size="lg"
+          color="gray.500"
+          variant="ghost"
+        />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/leejamesrobinson/"
+        title="LinkedIn"
+        isExternal
+      >
+        <IconButton
+          aria-label="LinkedIn"
+          icon="linkedin"
+          size="lg"
+          color="gray.500"
+          variant="ghost"
+        />
+      </Link>
+      <Link href="mailto:me@leerob.io" title="Email" isExternal>
+        <IconButton
+          aria-label="Email"
+          icon="mail"
+          size="lg"
+          color="gray.500"
+          variant="ghost"
+        />
+      </Link>
+    </div>
   </Flex>
 );
 
