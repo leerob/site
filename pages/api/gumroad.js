bed5ad957dd415033d65eef70e5b783fe1db6e58
Lee Robinson <lrobinson2011@gmail.com>
2020-07-24 19:57:21 -0500

    Add React 2025 to Gumroad API route. (#211)

--- pages/api/gumroad.js
+++ pages/api/gumroad.js
@@ -1,15 +1,24 @@
-export default async (_, res) => {
-  const API_KEY = process.env.GUMROAD_API_KEY;
-  const response = await fetch(`https://api.gumroad.com/v2/products/sDpG`, {
+import Big from 'big.js';
+
+const getProductSales = async (id) => {
+  const response = await fetch(`https://api.gumroad.com/v2/products/${id}`, {
     headers: {
-      Authorization: `Bearer ${API_KEY}`,
+      Authorization: `Bearer ${process.env.GUMROAD_API_KEY}`,
       'Content-Type': 'application/json'
     },
     method: 'GET'
   });
 
   const { product } = await response.json();
-  const sales = product.sales_usd_cents / 100;
 
-  return res.status(200).json({ sales });
+  return new Big(product.sales_usd_cents).div(100);
+};
+
+export default async (_, res) => {
+  const masteringNextSales = await getProductSales('sDpG');
+  const react2025Sales = await getProductSales('TifxZ');
+
+  return res.status(200).json({
+    sales: masteringNextSales.plus(react2025Sales).toFixed()
+  });
 };
