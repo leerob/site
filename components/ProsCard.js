c2c1d9f996feddfea1b618dd9a2bca3414bbcdbb
Lee Robinson <lrobinson2011@gmail.com>
2020-08-08 22:58:16 -0500

    Which Backend Should I Use As A Front-End Developer?

---
+++ components/ProsCard.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  List,
+  ListItem,
+  ListIcon,
+  Text,
+  Box,
+  useColorMode
+} from '@chakra-ui/core';
+
+const ProsCard = ({ title, pros }) => {
+  const { colorMode } = useColorMode();
+  const bgColor = {
+    light: 'green.50',
+    dark: 'green.900'
+  };
+  const borderColor = {
+    light: 'green.200',
+    dark: 'green.900'
+  };
+
+  return (
+    <Box
+      border="1px solid"
+      borderColor={borderColor[colorMode]}
+      bg={bgColor[colorMode]}
+      borderRadius={4}
+      padding={6}
+      my={4}
+      w="100%"
+    >
+      <Text>{`You might use ${title} if...`}</Text>
+      <List spacing={2} mt={4}>
+        {pros.map((pro) => (
+          <ListItem
+            key={pro}
+            fontWeight="medium"
+            // fontSize="lg"
+            alignItems="center"
+            display="flex"
+          >
+            <ListIcon icon="check" color="green.500" />
+            {pro}
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export default ProsCard;
--- styles/theme.js
+++ styles/theme.js
@@ -105,6 +105,34 @@ const theme = {
         />
       ),
       viewBox: '0 0 168 168'
+    },
+    check: {
+      path: (
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+          <path d="M22 4L12 14.01l-3-3" />
+        </g>
+      )
+    },
+    x: {
+      path: (
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="12" cy="12" r="10" />
+          <path d="M15 9l-6 6M9 9l6 6" />
+        </g>
+      )
     }
   }
 };
