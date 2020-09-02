fa7204893faa0748f338c69c5829533ad8a0838e
Lee Robinson <lrobinson2011@gmail.com>
2020-09-01 20:26:18 -0500

    Updates that work!

--- layouts/index.js
+++ layouts/index.js
@@ -24,90 +24,87 @@ const discussUrl = (slug) =>
     `https://leerob.io/blog/${slug}`
   )}`;
 
-export default (frontMatter) => {
+export default function BlogLayout({ children, frontMatter }) {
   const slug = frontMatter.__resourcePath
     .replace('blog/', '')
     .replace('.mdx', '');
+  const { colorMode } = useColorMode();
+  const textColor = {
+    light: 'gray.700',
+    dark: 'gray.400'
+  };
 
-  return ({ children }) => {
-    const { colorMode } = useColorMode();
-    const textColor = {
-      light: 'gray.700',
-      dark: 'gray.400'
-    };
-
-    return (
-      <Container>
-        <BlogSeo url={`https://leerob.io/blog/${slug}`} {...frontMatter} />
-        <Stack
-          as="article"
-          spacing={8}
-          justifyContent="center"
+  return (
+    <Container>
+      <BlogSeo url={`https://leerob.io/blog/${slug}`} {...frontMatter} />
+      <Stack
+        as="article"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        maxWidth="700px"
+        w="100%"
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
           alignItems="flex-start"
-          m="0 auto 4rem auto"
           maxWidth="700px"
           w="100%"
         >
+          <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
+            {frontMatter.title}
+          </Heading>
           <Flex
-            flexDirection="column"
-            justifyContent="flex-start"
-            alignItems="flex-start"
-            maxWidth="700px"
+            justify="space-between"
+            align={['initial', 'center']}
+            direction={['column', 'row']}
+            mt={2}
             w="100%"
+            mb={4}
           >
-            <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
-              {frontMatter.title}
-            </Heading>
-            <Flex
-              justify="space-between"
-              align={['initial', 'center']}
-              direction={['column', 'row']}
-              mt={2}
-              w="100%"
-              mb={4}
-            >
-              <Flex align="center">
-                <Avatar
-                  size="xs"
-                  name="Lee Robinson"
-                  src="https://bit.ly/33vEjhB"
-                  mr={2}
-                />
-                <Text fontSize="sm" color={textColor[colorMode]}>
-                  {frontMatter.by}
-                  {'Lee Robinson / '}
-                  {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
-                </Text>
-              </Flex>
-              <Text fontSize="sm" color="gray.500" minWidth="100px" mt={[2, 0]}>
-                {frontMatter.readingTime.text}
-                {` • `}
-                <ViewCounter id={slug} />
+            <Flex align="center">
+              <Avatar
+                size="xs"
+                name="Lee Robinson"
+                src="https://bit.ly/33vEjhB"
+                mr={2}
+              />
+              <Text fontSize="sm" color={textColor[colorMode]}>
+                {frontMatter.by}
+                {'Lee Robinson / '}
+                {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
               </Text>
             </Flex>
+            <Text fontSize="sm" color="gray.500" minWidth="100px" mt={[2, 0]}>
+              {frontMatter.readingTime.text}
+              {` • `}
+              <ViewCounter id={slug} />
+            </Text>
           </Flex>
-          {children}
-          <Subscribe />
-          <Box>
-            <Link href={discussUrl(slug)} isExternal>
-              {'Discuss on Twitter'}
-            </Link>
-            {` • `}
-            <Link href={editUrl(slug)} isExternal>
-              {'Edit on GitHub'}
-            </Link>
-          </Box>
-          <IframeResizer
-            checkOrigin={false}
-            title="Comments"
-            src={`https://fastfeedback.io/embed/BLspD6y8Bfn73LLm7nvW/${slug}?theme=${colorMode}`}
-            style={{
-              width: '1px',
-              minWidth: '100%'
-            }}
-          />
-        </Stack>
-      </Container>
-    );
-  };
-};
+        </Flex>
+        {children}
+        <Subscribe />
+        <Box>
+          <Link href={discussUrl(slug)} isExternal>
+            {'Discuss on Twitter'}
+          </Link>
+          {` • `}
+          <Link href={editUrl(slug)} isExternal>
+            {'Edit on GitHub'}
+          </Link>
+        </Box>
+        <IframeResizer
+          checkOrigin={false}
+          title="Comments"
+          src={`https://fastfeedback.io/embed/BLspD6y8Bfn73LLm7nvW/${slug}?theme=${colorMode}`}
+          style={{
+            width: '1px',
+            minWidth: '100%'
+          }}
+        />
+      </Stack>
+    </Container>
+  );
+}
