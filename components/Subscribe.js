c408f147cb11d9a381846f742c01d9c33db7110c
Lee Robinson <lrobinson2011@gmail.com>
2020-12-09 07:33:02 -0600

    Make email input required.

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -99,6 +99,7 @@ export default function Subscribe() {
           placeholder="tim@apple.com"
           type="email"
           autoComplete="email"
+          required
           className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
         />
         <button
