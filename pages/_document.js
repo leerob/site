f829fd54d9041deebb2379b4e1d0511f9d486186
Lee Robinson <lrobinson2011@gmail.com>
2020-12-11 15:45:51 -0600

    Improved Inter font loading.

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import GoogleFonts from 'next-google-fonts';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
         <Head>
           <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
           <link href="/static/favicons/site.webmanifest" rel="manifest" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com/"
-            crossOrigin=""
-          />
           <link
             rel="preconnect"
             href="https://cdn.usefathom.com"
