74a3ebb14b81f785ea96f9c73fbe4f6c2463679c
Lee Robinson <lrobinson2011@gmail.com>
2020-12-14 10:26:15 -0600

    Trying Preact only in the client production build.
    h/t https://blogify.dev/egoist/how-i-made-my-next-js-build-50-percent-smaller

--- next.config.js
+++ next.config.js
@@ -2,9 +2,18 @@ module.exports = {
   images: {
     domains: ['i.scdn.co'] // Spotify Album Art
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config, { dev, isServer }) => {
     if (isServer) {
       require('./scripts/generate-sitemap');
+    }
+
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      });
     }
 
     return config;
