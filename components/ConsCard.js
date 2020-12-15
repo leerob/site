f451f4541ccd86bf54346bc182c8753427247487
Lee Robinson <lrobinson2011@gmail.com>
2020-12-14 19:36:40 -0600

    Fix typo.

--- components/ConsCard.js
+++ components/ConsCard.js
@@ -1,7 +1,7 @@
 export default function ConsCard({ title, cons }) {
   return (
     <div className="border border-red-200 dark:border-red-900 bg-red-50 dark:bg-red-900 rounded p-6 my-6 w-full">
-      <span>{`You might not want to use ${title} if...`}</span>
+      <span>{`You might not use ${title} if...`}</span>
       <div className="mt-4">
         {cons.map((con) => (
           <div key={con} className="flex font-medium items-baseline mb-2">
