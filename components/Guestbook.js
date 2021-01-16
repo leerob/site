d968af57eeeb88f8f8352d2ececcdc3c497ea770
Lee Robinson <lrobinson2011@gmail.com>
2021-01-16 10:20:27 -0600

    Better padding on the input.

--- components/Guestbook.js
+++ components/Guestbook.js
@@ -98,7 +98,7 @@ export default function Guestbook({ initialEntries }) {
               aria-label="Your message"
               placeholder="Your message..."
               required
-              className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+              className="pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
             />
             <button
               className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
