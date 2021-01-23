1a03769a8e4e9b368a9ead7f08864c8628321127
Lee Robinson <lrobinson2011@gmail.com>
2021-01-23 17:26:12 -0600

    Add RSS feed.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = {
   webpack: (config, { dev, isServer }) => {
     if (isServer) {
       require('./scripts/generate-sitemap');
+      require('./scripts/generate-rss');
     }
 
     // Replace React with Preact only in client production build
---
+++ scripts/generate-rss.js
@@ -0,0 +1,34 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+const RSS = require('rss');
+const matter = require('gray-matter');
+
+async function generate() {
+  const feed = new RSS({
+    title: 'Lee Robinson',
+    site_url: 'https://leerob.io',
+    feed_url: 'https://leerob.io/feed.xml'
+  });
+
+  const posts = await fs.readdir(path.join(__dirname, '..', 'data', 'blog'));
+
+  await Promise.all(
+    posts.map(async (name) => {
+      const content = await fs.readFile(
+        path.join(__dirname, '..', 'data', 'blog', name)
+      );
+      const frontmatter = matter(content);
+
+      feed.item({
+        title: frontmatter.data.title,
+        url: 'https://leerob.io/blog/' + name.replace(/\.mdx?/, ''),
+        date: frontmatter.data.publishedAt,
+        description: frontmatter.data.summary
+      });
+    })
+  );
+
+  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }));
+}
+
+generate();
