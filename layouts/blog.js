e506334bed05b54fa47b9e0b28ad6d1bd25fc712
Árpád Illyés <13800404+arpi17@users.noreply.github.com>
2021-02-02 22:29:09 +0100

    Point edit URL to main branch

--- layouts/blog.js
+++ layouts/blog.js
@@ -6,7 +6,7 @@ import Subscribe from '@/components/Subscribe';
 import ViewCounter from '@/components/ViewCounter';
 
 const editUrl = (slug) =>
-  `https://github.com/leerob/leerob.io/edit/master/data/blog/${slug}.mdx`;
+  `https://github.com/leerob/leerob.io/edit/main/data/blog/${slug}.mdx`;
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
     `https://leerob.io/blog/${slug}`
