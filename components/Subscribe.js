bbbdd3739c235ed784f33ea3d1fda3c3672d5402
Lee Robinson <lrobinson2011@gmail.com>
2021-02-17 11:09:53 -0600

    Add latest newsletter.

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -80,7 +80,7 @@ export default function Subscribe() {
         <p className="text-sm text-gray-800 dark:text-gray-200">
           {`${subscriberCount || '-'} subscribers – `}
           <Link href="/newsletter">
-            <a>27 issues</a>
+            <a>28 issues</a>
           </Link>
         </p>
       )}
