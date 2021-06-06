68aeddc8d86631b8b1d98782ce2b762364746396
joaogarin <joao.garin@gmail.com>
2021-06-06 09:30:55 +0100

    add footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,5 @@
 import Link from 'next/link';
 
-import NowPlaying from '@/components/NowPlaying';
-
 const ExternalLink = ({ href, children }) => (
   <a
     className="text-gray-500 hover:text-gray-600 transition"
@@ -17,7 +15,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
       <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
-      <NowPlaying />
+
       <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
         <div className="flex flex-col space-y-4">
           <Link href="/">
