25a784e9b55d9a0b445a7ee5d8ca9124a2ecfea6
Lee Robinson <lrobinson2011@gmail.com>
2021-06-18 20:23:16 -0500

    Try upgrading.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 module.exports = {
   future: {
-    webpack5: true,
     strictPostcssConfiguration: true
   },
   reactStrictMode: true,
-  experimental: {
-    turboMode: true,
-    eslint: true
-  },
   images: {
     domains: [
       'i.scdn.co', // Spotify Album Art
