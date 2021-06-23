53df3c83881f75325f0bbafec8f8f82b2861991d
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2021-06-23 00:58:28 -0300

    Prevent render noscript img on client when js is available

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,10 @@ import '@/styles/global.css';
 import { ThemeProvider } from 'next-themes';
 import { MDXProvider } from '@mdx-js/react';
 import { useAnalytics } from '@/lib/analytics';
+import { patchPreact } from '@/lib/patch-preact';
 import MDXComponents from '@/components/MDXComponents';
+
+patchPreact();
 
 export default function App({ Component, pageProps }) {
   useAnalytics();
