c12feb3d6db64284fc9be33328a7066ec84d5cc0
joaogarin <joao.garin@gmail.com>
2021-06-28 21:15:11 +0100

    fix twitter site

--- components/Container.js
+++ components/Container.js
@@ -40,7 +40,7 @@ export default function Container(props) {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@leeerob" />
+        <meta name="twitter:site" content="@joaogarin" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
