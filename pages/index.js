f80ca89e003286f487c061c4c058b892a08af7bd
joaogarin <joao.garin@gmail.com>
2021-07-08 16:54:41 +0100

    add avatar in home

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Container from '../components/Container';
 import BlogPost from '../components/BlogPost';
 import ProjectCard from '../components/ProjectCard';
+import Image from 'next/image';
 
 export async function getStaticProps() {
   return {
@@ -14,17 +15,38 @@ export default function Home({ videos }) {
   return (
     <Container>
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white ">
-          Hey, I’m Joao Garin
-        </h1>
-        <h2 className="prose text-gray-600 dark:text-gray-400 mb-16">
-          I'm a frontend developer, creator and javascript enthusiast. I work as
-          a senior Frontend Developer at jobiqo. You’ve found my personal slice
-          of the internet –&nbsp;
-          <Link href="/about">
-            <a>learn more about me.</a>
-          </Link>
-        </h2>
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2">
+          <div className="flex items-center">
+            <div className="mr-2">
+              <Image
+                alt="Joao Garin"
+                height={100}
+                width={100}
+                src="/avatar.jpg"
+                className="rounded-full"
+              />
+            </div>
+            <div className="text-sm text-gray-700 dark:text-gray-300 ml-2">
+              <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white ">
+                Joao's digital garden
+              </h1>
+              <div className="prose text-gray-600 dark:text-gray-400">
+                Hi there, I am Joao. A Frontend developer, builder and
+                javascript enthusiast. I work as a senior Frontend Developer at{' '}
+                <Link href="https://www.jobiqo.com/">
+                  <a>jobiqo</a>
+                </Link>
+                . Congratulations, you found my digital garden, here have a 🍺
+                it's on me.
+                <br />
+                <Link href="/about">
+                  <a>Learn more about me.</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mb-16" />
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
           Latest articles
         </h3>
