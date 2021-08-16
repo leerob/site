629c03d0c96b495a1d16ffa14f6a4a6c72a4f6f4
Lee Robinson <lrobinson2011@gmail.com>
2021-08-16 00:35:38 -0500

    Update sitemap post
    Closes https://github.com/leerob/leerob.io/issues/293

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -14,25 +14,25 @@ import prettier from 'prettier';
   ]);
 
   const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map((page) => {
-                const path = page
-                  .replace('pages', '')
-                  .replace('data', '')
-                  .replace('.js', '')
-                  .replace('.mdx', '');
-                const route = path === '/index' ? '' : path;
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+          .map((page) => {
+            const path = page
+              .replace('pages', '')
+              .replace('data', '')
+              .replace('.js', '')
+              .replace('.mdx', '');
+            const route = path === '/index' ? '' : path;
 
-                return `
-                    <url>
-                        <loc>${`https://leerob.io${route}`}</loc>
-                    </url>
-                `;
-              })
-              .join('')}
-        </urlset>
+            return `
+              <url>
+                  <loc>${`https://leerob.io${route}`}</loc>
+              </url>
+            `;
+          })
+          .join('')}
+    </urlset>
     `;
 
   const formatted = prettier.format(sitemap, {
