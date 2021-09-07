620646c139f2a3464a213eb7ab797f8444337287
Lee Robinson <lrobinson2011@gmail.com>
2021-09-07 17:01:31 -0500

    Make APIs work

--- pages/api/subscribers.js
+++ pages/api/subscribers.js
@@ -1,19 +1,20 @@
-export default async function handler(_, res) {
-  const API_KEY = process.env.REVUE_API_KEY;
-  const response = await fetch('https://www.getrevue.co/api/v2/lists', {
+export default async function handler(req, res) {
+  const result = await fetch('https://www.getrevue.co/api/v2/subscribers', {
+    method: 'GET',
     headers: {
-      Authorization: `Token ${API_KEY}`,
-      'Content-Type': 'application/json'
-    },
-    method: 'GET'
+      Authorization: `Token ${process.env.REVUE_API_KEY}`
+    }
   });
+  const data = await result.json();
 
-  const test = await response.json();
+  if (!result.ok) {
+    return res.status(500).json({ error: 'Error retrieving subscribers' });
+  }
 
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=1200, stale-while-revalidate=600'
   );
 
-  return res.status(200).json({ test });
+  return res.status(200).json({ count: data.length });
 }
