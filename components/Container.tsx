271c1ae2c088c9fb9c30d5d9467d13eafa1dd677
Lee Robinson <lrobinson2011@gmail.com>
2021-10-02 22:43:53 -0500

    Redesign.

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,6 +5,7 @@ import { useTheme } from 'next-themes';
 import NextLink from 'next/link';
 
 import Footer from 'components/Footer';
+import MobileMenu from 'components/MobileMenu';
 
 export default function Container(props) {
   const [mounted, setMounted] = useState(false);
@@ -24,7 +25,7 @@ export default function Container(props) {
   };
 
   return (
-    <div className="bg-white dark:bg-black">
+    <div className="bg-gray-50 dark:bg-gray-900">
       <Head>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
@@ -45,23 +46,56 @@ export default function Container(props) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 dark:bg-black bg-opacity-60 dark:text-gray-100">
+      <nav className="flex items-center justify-between w-full max-w-3xl mx-auto pt-4 pb-8 text-gray-900 bg-gray-50 sticky-nav md:my-8 dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
         <a href="#skip" className="skip-nav">
           Skip to content
         </a>
+        <div className="ml-6">
+          <MobileMenu />
+          <NextLink href="/">
+            <a className="hidden md:inline-flex p-1 font-semibold text-gray-800 sm:p-3 sm:p-1 dark:text-gray-200">
+              Home
+            </a>
+          </NextLink>
+          <NextLink href="/guestbook">
+            <a className="hidden md:inline-flex p-1 font-normal text-gray-800 sm:p-3 dark:text-gray-300">
+              Guestbook
+            </a>
+          </NextLink>
+          <NextLink href="/dashboard">
+            <a className="hidden md:inline-flex p-1 font-normal text-gray-800 sm:p-3 dark:text-gray-300">
+              Dashboard
+            </a>
+          </NextLink>
+          <NextLink href="/videos">
+            <a className="hidden md:inline-flex p-1 font-normal text-gray-800 sm:p-3 dark:text-gray-300">
+              Videos
+            </a>
+          </NextLink>
+          <NextLink href="/blog">
+            <a className="hidden md:inline-flex p-1 font-normal text-gray-800 sm:p-3 dark:text-gray-300">
+              Blog
+            </a>
+          </NextLink>
+          <NextLink href="/snippets">
+            <a className="hidden md:inline-flex p-1 font-normal text-gray-800 sm:p-3 dark:text-gray-300">
+              Snippets
+            </a>
+          </NextLink>
+        </div>
         <button
           aria-label="Toggle Dark Mode"
           type="button"
-          className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800"
+          className="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center mr-6"
           onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
         >
           {mounted && (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              fill="currentColor"
+              fill="none"
               stroke="currentColor"
-              className="w-4 h-4 text-gray-800 dark:text-gray-200"
+              className="w-5 h-5 text-gray-800 dark:text-gray-200"
             >
               {resolvedTheme === 'dark' ? (
                 <path
@@ -75,32 +109,16 @@ export default function Container(props) {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
                 />
               )}
             </svg>
           )}
         </button>
-        <div>
-          <NextLink href="/dashboard">
-            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">
-              Dashboard
-            </a>
-          </NextLink>
-          <NextLink href="/blog">
-            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Blog</a>
-          </NextLink>
-          <NextLink href="/about">
-            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">About</a>
-          </NextLink>
-          <NextLink href="/">
-            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Home</a>
-          </NextLink>
-        </div>
       </nav>
       <main
         id="skip"
-        className="flex flex-col justify-center px-8 bg-white dark:bg-black"
+        className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900"
       >
         {children}
         <Footer />
