aa4b63728903131fb8094761db168bef2815ccbc
Lee Robinson <lrobinson2011@gmail.com>
2021-10-26 01:27:24 -0500

    Turn off minify

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const { withContentlayer } = require('next-contentlayer');
  * @type {import('next').NextConfig}
  */
 module.exports = withContentlayer()({
-  swcMinify: true,
+  swcMinify: false,
   reactStrictMode: true,
   images: {
     domains: [
