456cc8a56741062c96ff7a367c9419faa60c7c52
Lee Robinson <lrobinson2011@gmail.com>
2021-10-26 07:02:06 -0500

    Readd headers.

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -15,22 +15,22 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   const response = NextResponse.next();
 
-  // response.headers.set(
-  //   'Content-Security-Policy',
-  //   ContentSecurityPolicy.replace(/\n/g, '')
-  // );
+  response.headers.set(
+    'Content-Security-Policy',
+    ContentSecurityPolicy.replace(/\n/g, '')
+  );
   response.headers.set('Referrer-Policy', 'origin-when-cross-origin');
-  // response.headers.set(
-  //   'Permissions-Policy',
-  //   'camera=(), microphone=(), geolocation=()'
-  // );
-  // response.headers.set(
-  //   'Strict-Transport-Security',
-  //   'max-age=31536000; includeSubDomains; preload'
-  // );
-  // response.headers.set('X-Frame-Options', 'DENY');
-  // response.headers.set('X-Content-Type-Options', 'nosniff');
-  // response.headers.set('X-DNS-Prefetch-Control', 'on');
+  response.headers.set(
+    'Permissions-Policy',
+    'camera=(), microphone=(), geolocation=()'
+  );
+  response.headers.set(
+    'Strict-Transport-Security',
+    'max-age=31536000; includeSubDomains; preload'
+  );
+  response.headers.set('X-Frame-Options', 'DENY');
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response.headers.set('X-DNS-Prefetch-Control', 'on');
 
   return response;
 }
