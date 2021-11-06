6f4ddbb390ca513dab32ecc75ce6fc8b05b9902b
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2021-11-07 01:55:59 +0530

    fix(subscribe): padding right added to input field (#403)

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -63,7 +63,7 @@ export default function Subscribe() {
           type="email"
           autoComplete="email"
           required
-          className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+          className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 pr-32"
         />
         <button
           className="flex items-center justify-center absolute right-1 top-1 px-4 pt-1 font-medium h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
