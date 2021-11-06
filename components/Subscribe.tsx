ad049823bcda8f8821877763c68ee60de446527b
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2021-11-06 03:16:28 +0000

    fix(subscribe): padding right added to input field

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,13 +1,12 @@
-import { useState, useRef } from 'react';
-import Link from 'next/link';
-import useSWR from 'swr';
-import { trackGoal } from 'fathom-client';
-
-import fetcher from 'lib/fetcher';
-import { Form, FormState, Subscribers } from 'lib/types';
-import SuccessMessage from 'components/SuccessMessage';
 import ErrorMessage from 'components/ErrorMessage';
 import LoadingSpinner from 'components/LoadingSpinner';
+import SuccessMessage from 'components/SuccessMessage';
+import { trackGoal } from 'fathom-client';
+import fetcher from 'lib/fetcher';
+import { Form, FormState, Subscribers } from 'lib/types';
+import Link from 'next/link';
+import { useRef, useState } from 'react';
+import useSWR from 'swr';
 
 export default function Subscribe() {
   const [form, setForm] = useState<FormState>({ state: Form.Initial });
@@ -63,7 +62,7 @@ export default function Subscribe() {
           type="email"
           autoComplete="email"
           required
-          className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+          className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 pr-32"
         />
         <button
           className="flex items-center justify-center absolute right-1 top-1 px-4 pt-1 font-medium h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
