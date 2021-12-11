4a7a9df6ba6205566eb32371ccb858b8ce9da1d1
Lee Robinson <lrobinson2011@gmail.com>
2021-12-10 21:09:21 -0600

    Fix broken auth.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
 export default NextAuth({
+  secret: process.env.NEXT_AUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.OAUTH_CLIENT_KEY,
