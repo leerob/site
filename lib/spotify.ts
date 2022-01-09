e0b970e8dba0866aa8debbcdf23eb9e86d8025a6
Oliver Parkinson <olliepar01@gmail.com>
2022-01-09 01:39:47 +0000

    Swaped out qs for URLSearchParams out of the box from node

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,5 +1,3 @@
-import qs from 'qs';
-
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
@@ -16,10 +14,10 @@ const getAccessToken = async () => {
       Authorization: `Basic ${basic}`,
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: qs.stringify({
+    body: new URLSearchParams({
       grant_type: 'refresh_token',
-      refresh_token
-    })
+      refresh_token,
+    }),
   });
 
   return response.json();
