6c021e5aadbcf1226ea21dee225ca9a359aaaa3a
Lee Robinson <lrobinson2011@gmail.com>
2022-06-19 18:49:20 -0500

    Disable preact for now, rip

--- next.config.js
+++ next.config.js
@@ -26,14 +26,14 @@ module.exports = {
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
 
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      });
-    }
+    // if (!dev && !isServer) {
+    //   Object.assign(config.resolve.alias, {
+    //     'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+    //     react: 'preact/compat',
+    //     'react-dom/test-utils': 'preact/test-utils',
+    //     'react-dom': 'preact/compat'
+    //   });
+    // }
 
     return config;
   }
