9c202d6fde3e1e1787a0b5f3bd4729ab99427af6
Lee Robinson <lrobinson2011@gmail.com>
2022-06-20 12:38:20 -0500

    VP of Developer Experience 🚀  (#498)

--- pages/about.tsx
+++ pages/about.tsx
@@ -36,19 +36,19 @@ export default function About() {
           </ul>
           <h2>Bio</h2>
           <h3>Job Title</h3>
-          <p>Lee Robinson, Director of Developer Relations at Vercel</p>
+          <p>Lee Robinson, VP of Developer Experience at Vercel</p>
           <h3>Long, 3rd Person</h3>
           <p>
-            Lee Robinson is the Director of Developer Relations at{' '}
+            Lee Robinson is the VP of Developer Experience at{' '}
             <a href="http://vercel.com/">Vercel</a>, where he helps developers
-            build a faster web and leads the Next.js community. Prior to that,
-            Lee was a Senior Software Engineer focused on the frontend. An
-            educator, writer, and speaker, Lee has also created extensive
-            courses on React and Next.js.
+            build a faster web and leads the Next.js community. He leads the
+            community for the React framework Next.js and is an open-source
+            contributor. An educator, writer, and speaker, Lee has created
+            courses on React, Next.js, and web development.
           </p>
           <h3>Long, 1st Person</h3>
           <p>
-            Hey, I'm Lee. I lead Developer Relations at{' '}
+            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
             <a href="https://vercel.com/">Vercel</a>, where my team helps
             developers build a faster web. I'm a Next.js contributor and help
             lead our open-source communities. I've passionate about frontend
@@ -57,19 +57,19 @@ export default function About() {
           </p>
           <h3>Short, 3rd Person</h3>
           <p>
-            Lee Robinson is the Director of Developer Relations at{' '}
+            Lee Robinson is the VP of Developer Experience at{' '}
             <a href="http://vercel.com/">Vercel</a>, where he helps developers
             build a faster web.
           </p>
           <h3>Short, 1st Person</h3>
           <p>
-            Hey, I'm Lee. I lead Developer Relations at{' '}
+            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
             <a href="https://vercel.com/">Vercel</a>, where my team helps
             developers build a faster web.
           </p>
           <h3>Education</h3>
           <p>
-            Lee Robinson graduated from Iowa State University with a BS in
+            Lee Robinson graduated from Iowa State University with a B.S. in
             Computer Engineering.
           </p>
           <h2>Headshots</h2>
