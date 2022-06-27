7184c875df2fca40a2637e8b08fbe4804d6649f1
Dzmitry Sviryn <zvirinz@outlook.com>
2022-06-27 18:32:49 +0300

    ci(tooling): add husky, lint-staged, commitlint

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+  '**/*.ts?(x)': () => 'yarn  check-types',
+  '*.json': ['prettier --write']
+};
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  changefreq: 'weekly',
+  priority: 0.7
+};
