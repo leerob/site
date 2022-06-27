66b49032c7f9fa7a555f1e999d15d50f9cb110b4
Dzmitry Sviryn <zvirinz@outlook.com>
2022-06-27 20:36:10 +0300

    feat(db): connect prisma to plantscale

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
 import { usePanelbear } from "@panelbear/panelbear-nextjs";
+import { Analytics } from "components/Analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
     usePanelbear(process.env.NEXT_PUBLIC_PANELBEAR_SITE_ID, {
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
+        <Analytics />
         <Component {...pageProps} />
       </ThemeProvider>
     </SessionProvider>
