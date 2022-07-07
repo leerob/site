25796e38a41c231a145b7408f107a8b928552773
Dzmitry Sviryn <zvirinz@outlook.com>
2022-07-07 22:53:25 +0300

    fix(about-page): svg

--- src/components/TopSkills.tsx
+++ src/components/TopSkills.tsx
@@ -1,105 +1,25 @@
 import { useMemo } from 'react';
 
-import {
-  SiAlgolia,
-  SiAmazonaws,
-  SiDocker,
-  SiExpress,
-  SiFigma,
-  SiFirebase,
-  SiGraphql,
-  SiHeroku,
-  SiJest,
-  SiLinux,
-  SiMapbox,
-  SiPostgresql,
-  SiPrisma,
-  SiPnpm,
-  SiReact,
-  SiSupabase,
-  SiTailwindcss,
-  SiVercel
-} from 'react-icons/si';
-import ExternalLink from '@/components/ExternalLink';
+import Nextjs from '@/assets/nextjs.svg';
+import Typescript from '@/assets/typescript.svg';
+import Sanity from '@/assets/sanity.svg';
 
-// import Sanity from '/sanity.svg';
-// import Nextjs from '/nextjs.svg';
-// import Typescript from '/yypescript.svg';
+import ExternalLink from '@/components/ExternalLink';
 
 export default function TopSkills() {
   const stacks = useMemo(
     () => [
       {
-        Icon: SiAlgolia,
+        Icon: Nextjs,
         url: 'https://golang.org/'
       },
       {
-        Icon: SiAmazonaws,
+        Icon: Typescript,
         url: 'https://golang.org/'
       },
       {
-        Icon: SiDocker,
+        Icon: Sanity,
         url: 'https://kubernetes.io/'
-      },
-      {
-        Icon: SiExpress,
-        url: 'https://www.typescriptlang.org/'
-      },
-      {
-        Icon: SiFigma,
-        url: 'https://graphql.org/'
-      },
-      {
-        Icon: SiFirebase,
-        url: 'https://aws.amazon.com/'
-      },
-      {
-        Icon: SiGraphql,
-        url: 'https://nextjs.org/'
-      },
-      {
-        Icon: SiHeroku,
-        url: 'https://elixir-lang.org/'
-      },
-      {
-        Icon: SiJest,
-        url: 'https://cloud.google.com/'
-      },
-      {
-        Icon: SiLinux,
-        url: 'https://www.terraform.io/'
-      },
-      {
-        Icon: SiMapbox,
-        url: 'https://www.postgresql.org/'
-      },
-      {
-        Icon: SiPostgresql,
-        url: 'https://www.python.org/'
-      },
-      {
-        Icon: SiPrisma,
-        url: 'https://www.python.org/'
-      },
-      {
-        Icon: SiPnpm,
-        url: 'https://www.python.org/'
-      },
-      {
-        Icon: SiReact,
-        url: 'https://www.python.org/'
-      },
-      {
-        Icon: SiSupabase,
-        url: 'https://www.python.org/'
-      },
-      {
-        Icon: SiTailwindcss,
-        url: 'https://www.python.org/'
-      },
-      {
-        Icon: SiVercel,
-        url: 'https://www.python.org/'
       }
     ],
     []
@@ -109,7 +29,7 @@ export default function TopSkills() {
     <div className="grid-cols-8 gap-4 justify-center">
       {stacks.map(({ Icon, url }) => (
         <ExternalLink href={url} key={url}>
-          <Icon className="w-8 h-8" />
+          <Icon />
         </ExternalLink>
       ))}
     </div>
