7a64dbf1eacd5c8c84cd0c73db3c45f1b9c9fa46
Lee Robinson <lrobinson2011@gmail.com>
2022-07-11 09:48:40 -0500

    Fix Spotify now playing.

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -2,7 +2,7 @@ const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
 
-const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+const basic = btoa(`${client_id}:${client_secret}`);
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
