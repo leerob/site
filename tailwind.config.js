e653e2d9bbb6a5a1a540971d58be2a9825e1a3fe
zvirinz <zvirinz@outlook.com>
2022-07-24 17:37:15 +0300

    refactor: prose styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-
 const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   darkMode: 'class',
   theme: {
+    colors: {
+      'signal-light': 'rgb(0 204 51 / 75%)',
+      signal: 'rgb(0 204 51 / 85%)',
+      'signal-dark': 'rgb(0 204 51 / 95%)',
+      gray: colors.slate,
+      emerald: colors.emerald,
+      pink: colors.pink
+    },
+    fontFamily: {
+      sans: ['IBM Plex Sans', ...fontFamily.sans]
+    },
     extend: {
-      colors: {
-        'signal-light': 'rgb(0 204 51 / 75%)',
-        signal: 'rgb(0 204 51 / 85%)',
-        'signal-dark': 'rgb(0 204 51 / 95%)',
-        gray: {
-          0: '#fff',
-          50: '#F8FAFC',
-          100: '#F1F5F9',
-          200: '#E2E8F0',
-          300: '#CBD5E1',
-          400: '#94A3B8',
-          500: '#64748B',
-          600: '#475569',
-          700: '#334155',
-          800: '#1E293B',
-          900: '#0F172A'
-        }
-      },
-      fontFamily: {
-        sans: ['IBM Plex Sans', ...fontFamily.sans]
-      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
