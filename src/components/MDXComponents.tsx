a4c409e6092dbbccc06ffc74049ce35c215d478f
zvirinz <zvirinz@outlook.com>
2022-08-12 14:59:40 +0300

    chore: bump deps

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -2,7 +2,11 @@ import Image from '@/components/Image';
 
 const Underlined = ({ title, href }: { title: string; href: string }) => {
   return (
-    <a className=" text-gray-800 dark:text-gray-300  font-medium link-underline link-underline-gradient" href={href} title={title}>
+    <a
+      className=" text-gray-800 dark:text-gray-300  font-medium link-underline link-underline-gradient"
+      href={href}
+      title={title}
+    >
       {title}
     </a>
   );
