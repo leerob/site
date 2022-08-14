e110ba4efe8fb4661346e744f2fe746b3ac6c862
zvirinz <zvirinz@outlook.com>
2022-08-14 22:58:40 +0300

    feat: seo improvements

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,18 +11,30 @@ export const AUTHOR = {
 };
 
 export const DEFAULT_SEO = {
-  title: 'Developer portfolio site',
-  description: 'Default description',
+  title: `Dzmitry Sviryn's personal website`,
+  description:
+    'A full-stack developer passionate about React ecosystem, TypeScript and serverless backends',
+  canonical: 'https://www.svirins.codes/',
   openGraph: {
-    titleTemplate: 'Default description',
-    description: 'Default description.',
+    title: `Dzmitry Sviryn's personal website`,
+    description:
+      'A full-stack developer passionate about React ecosystem, TypeScript and serverless backends.',
     type: 'website',
     url: 'https://www.svirins.codes/',
-    site_name: 'Default description'
+    site_name: 'Svirins Codes',
+    images: [
+      {
+        url: 'https://www.svirins.codes/social-banner.webp',
+        width: 1200,
+        height: 675,
+        alt: 'My cat and my gear',
+        type: 'image/webp'
+      }
+    ]
   },
   twitter: {
     handle: '@svirins',
-    site: '@site',
+    site: '@svirins',
     cardType: 'summary_large_image'
   }
 };
