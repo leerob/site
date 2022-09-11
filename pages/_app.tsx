9f53c396ac3be6cd077b1b36669250d0607afecb
Lee Robinson <lrobinson2011@gmail.com>
2022-09-10 21:23:41 -0500

    Fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import 'styles/global.css';
 
-import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+}) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
