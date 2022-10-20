7834273e3f5c36c59539a6bb1ac2147d0313d0cc
svirins <svirins@gmail.com>
2022-10-21 02:57:41 +0400

    fix: sanity config

--- src/config/index.ts
+++ src/config/index.ts
@@ -40,8 +40,8 @@ export const DEFAULT_SEO = {
 };
 
 export const SANITY_CONFIG = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: 'production',
+  projectId: 'c8glljln',
   useCdn: process.env.NODE_ENV !== 'production',
   apiVersion: '2021-03-25'
 };
