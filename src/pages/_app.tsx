037af5ef981e492229dc47015d3cd5a29fd05be7
M00K <m00k@pop-os.localdomain>
2023-01-22 21:56:39 +0300

    feat: add vercel-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,31 @@
 import '@/styles/global.css';
 
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 import ErrorBoundary from '@/components/ErrorBoundary';
 
 import { DEFAULT_SEO } from '@/config';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.goatcounter.count({
-        path: router.asPath
-      });
-    }
-  }, [router]);
+  // const router = useRouter();
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     window.goatcounter.count({
+  //       path: router.asPath
+  //     });
+  //   }
+  // }, [router]);
   return (
     <ThemeProvider attribute="class">
       <DefaultSeo {...DEFAULT_SEO} />
       <ErrorBoundary>
         <Component {...pageProps} />
+        <Analytics />
       </ErrorBoundary>
     </ThemeProvider>
   );
