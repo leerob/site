4982b9c13079c4e9c022ab53ebe3156d33d67dd2
svirins <svirins@gmail.com>
2023-01-23 09:26:31 +0300

    feat: before moving to next-js 13

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import '@/styles/global.css';
 
 import type { AppProps } from 'next/app';
-// import { useRouter } from 'next/router';
 import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
-// import { useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 
 import ErrorBoundary from '@/components/ErrorBoundary';
@@ -12,14 +10,6 @@ import ErrorBoundary from '@/components/ErrorBoundary';
 import { DEFAULT_SEO } from '@/config';
 
 export default function App({ Component, pageProps }: AppProps) {
-  // const router = useRouter();
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined') {
-  //     window.goatcounter.count({
-  //       path: router.asPath
-  //     });
-  //   }
-  // }, [router]);
   return (
     <ThemeProvider attribute="class">
       <DefaultSeo {...DEFAULT_SEO} />
