799816157611a48aa36537235f5f545bb1976075
svirins <svirins@gmail.com>
2023-01-27 12:06:59 +0300

    refactor(appdir): snippet and single blog pages

--- app/snippets/[slug]/page.tsx
+++ app/snippets/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { MDXRemote } from 'next-mdx-remote';
 import { IconContext } from 'react-icons';
+import { ArticleJsonLd } from 'next-seo';
 
 import components from 'ui/MDXComponents';
 import { mdxToHtml } from 'lib/mdx';
@@ -7,75 +8,58 @@ import { getSnippet, getSnippetSlugs } from 'lib/sanity-api';
 
 import StackIcon from 'ui/StackIcon';
 
-// title={`${snippet.title} - Code Snippet`}
-// description="A collection of code snippets – including serverless functions, Node.js scripts, and CSS tricks."
-export default function SnippetsPage() {
-  return (
-    <IconContext.Provider
-      value={{
-        className: 'w-7 h-7 md:w-8 md:h-8 fill-gray-900  dark:fill-gray-100'
-      }}
-    >
-      <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
-        <div className="flex justify-between w-full">
-          <div>
-            <h1 className="font-bold capsize text-3xl  md:text-5xl tracking-tight mb-4 text-gray-900 dark:text-gray-100">
-              {snippet.title}
-            </h1>
-            <p className="text-gray-800 dark:text-gray-200 py-2 md:text-lg">
-              {snippet.description}
-            </p>
-          </div>
-          <div className="mt-2 md:mt-6 scale-125 md:scale-150 ml-4 origin-center">
-            <IconContext.Provider
-              value={{
-                className: 'w-9 h-9 fill-gray-900  dark:fill-gray-100'
-              }}
-            >
-              <StackIcon iconTitle={snippet.iconTitle!} />
-            </IconContext.Provider>
-          </div>
-        </div>
-        {/* <hr className="w-full border-1 max-w-2xl border-gray-200 dark:border-gray-800" /> */}
-
-        <div className="prose dark:prose-invert prose-slate  max-w-2xl  w-full md:prose-lg">
-          <MDXRemote
-            {...snippet.mdxContent!}
-            components={
-              {
-                ...components
-              } as any
-            }
-          />
-        </div>
-      </article>
-    </IconContext.Provider>
-  );
-}
-
-export async function getStaticPaths() {
+export async function generateStaticParams() {
   const paths = await getSnippetSlugs();
-  return {
-    paths: paths.map((slug) => ({ params: { slug } })),
-    fallback: 'blocking'
-  };
+  return paths.map((slug) => ({ slug: slug }));
 }
 
-export async function getStaticProps({ params }: { params: IParams }) {
+export default async function SnippetsPage({ params }) {
   const snippet = await getSnippet(params.slug);
-
-  if (!snippet) {
-    return { notFound: true };
-  }
-
   const { html } = await mdxToHtml(snippet.content);
 
-  return {
-    props: {
-      snippet: {
-        ...snippet,
-        mdxContent: html
-      }
-    }
-  };
+  return (
+    <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
+      <ArticleJsonLd
+        useAppDir={true}
+        type="Article"
+        url="https://example.com/blog"
+        title={`${snippet.title} Dzmitry Svirin`}
+        authorName="Dzmitry Svirin"
+        description="A collection of code snippets – including serverless functions, Node.js scripts, and CSS tricks"
+        images={[]}
+        datePublished={''}
+      />
+      <div className="flex justify-between w-full">
+        <div>
+          <h1 className="font-bold capsize text-3xl  md:text-5xl tracking-tight mb-4 text-gray-900 dark:text-gray-100">
+            {snippet.title}
+          </h1>
+          <p className="text-gray-800 dark:text-gray-200 py-2 md:text-lg">
+            {snippet.description}
+          </p>
+        </div>
+        <div className="mt-2 md:mt-6 scale-125 md:scale-150 ml-4 origin-center">
+          <IconContext.Provider
+            value={{
+              className: 'w-9 h-9 fill-gray-900  dark:fill-gray-100'
+            }}
+          >
+            <StackIcon iconTitle={snippet.iconTitle!} />
+          </IconContext.Provider>
+        </div>
+      </div>
+      {/* <hr className="w-full border-1 max-w-2xl border-gray-200 dark:border-gray-800" /> */}
+
+      <div className="prose dark:prose-invert prose-slate  max-w-2xl  w-full md:prose-lg">
+        <MDXRemote
+          {...html!}
+          components={
+            {
+              ...components
+            } as any
+          }
+        />
+      </div>
+    </article>
+  );
 }
