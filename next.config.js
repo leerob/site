9d67c814235a0788dc8960571a3d9009f2cd72d5
Lee Robinson <lrobinson2011@gmail.com>
2023-01-29 13:07:52 -0600

    Fix images.

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'pbs.twimg.com',
-        pathname: '/profile_images/**',
+        pathname: '/**',
       },
     ],
   },
