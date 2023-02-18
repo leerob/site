1356f8aa1adb083d5b192c6f53fa04946afefc00
Dev-Siri <102754004+Dev-Siri@users.noreply.github.com>
2023-02-19 00:55:09 +0530

    Remove unnecessary `className` attribute. (#581)

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default async function HomePage() {
   }
 
   return (
-    <section className="">
+    <section>
       <h1 className="font-bold text-3xl font-serif">{name}</h1>
       <p className="my-5 max-w-[460px] text-neutral-800 dark:text-neutral-200">
         {about()}
