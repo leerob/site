c2a4d8535ac88889115115ba9a611efd7aeabe44
Lee Robinson <lrobinson2011@gmail.com>
2023-02-25 16:11:06 -0600

    Revert "Fixes"
    This reverts commit 9691d7956d604e828f4d3d6bf7a3c49a46b84f29.

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,18 +1,22 @@
-import me from '../app/placeholder.jpg';
+import me from '../app/avatar.jpg';
 
-export const name = 'Your Name';
+export const name = 'Lee Robinson';
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as a Software Engineer.
+      Hey, I'm Lee. I'm the <b>VP of Developer Experience at Vercel</b> where my
+      team helps developers build a faster web.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
+      I'm a Next.js contributor and help lead our open-source communities. I'm
+      passionate about frontend development and have created courses on React,
+      Next.js, and web development. I'm an advisor and investor in early stage
+      startups.
     </>
   );
 };
