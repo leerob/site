ac5f940f2c239b8760dc7b25676faa6a955a4ef9
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-03-09 16:42:58 +0200

    styles: hidden bar scroll in nav (#589)

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -90,7 +90,7 @@ export default function Navbar() {
           <Logo />
         </div>
         <nav
-          className="flex flex-row md:flex-col items-start relative overflow-scroll px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex overflow-hidden flex-row md:flex-col items-start relative overflow-scroll px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
           <div className="flex flex-row md:flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0">
