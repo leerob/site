6f7bfe64060988578854c66e573348be54029bd0
Lee Robinson <lrobinson2011@gmail.com>
2023-07-20 20:19:55 -0500

    Don't zoom input on mobile

--- app/guestbook/form.tsx
+++ app/guestbook/form.tsx
@@ -11,7 +11,7 @@ export default function Form() {
   return (
     <form
       style={{ opacity: !pending ? 1 : 0.7 }}
-      className="relative max-w-[500px] text-sm"
+      className="relative max-w-[500px]"
       ref={formRef}
       action={async (formData) => {
         await saveGuestbookEntry(formData);
@@ -28,7 +28,7 @@ export default function Form() {
         className="pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
       />
       <button
-        className="flex items-center justify-center absolute right-1 top-1 px-2 py-1 font-medium h-7 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16"
+        className="flex items-center justify-center absolute right-1 top-1 px-2 py-1 font-medium h-8 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16"
         disabled={pending}
         type="submit"
       >
