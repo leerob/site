0e7558e10a0fe3c0a4bd2cb914072a74ab8f2726
Z4NR34L <software@zanreal.net>
2023-11-22 08:33:51 +0100

    Update app/api/blog/feed.xml/route.ts
    Co-authored-by: CodiumAI-Agent <137281646+CodiumAI-Agent@users.noreply.github.com>

--- app/api/blog/feed.xml/route.ts
+++ app/api/blog/feed.xml/route.ts
@@ -4,27 +4,33 @@ import { allBlogs } from "contentlayer/generated";
 const siteUrl = 'https://leerob.io';
 
 export async function GET(request: Request) {
-  const feed = new Rss({
-    title: "Lee Robinson",
-    description: "Developer, writer, and creator.",
-    feed_url: `${siteUrl}/api/blog/feed.xml`,
-    site_url: siteUrl,
-    language: "en",
-  });
-  
-  allBlogs.forEach((post) => {
-    feed.item({
-      title: post.title,
-      description: post.summary,
-      url: `${siteUrl}/blog/${post.slug}`,
-      date: post.publishedAt,
+  try {
+    const feed = new Rss({
+      title: "Lee Robinson",
+      description: "Developer, writer, and creator.",
+      feed_url: `${siteUrl}/api/blog/feed.xml`,
+      site_url: siteUrl,
+      language: "en",
     });
-  });
-  
-  return new Response(feed.xml(), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/xml'
-    }
-  })
+    
+    allBlogs.forEach((post) => {
+      feed.item({
+        title: post.title,
+        description: post.summary,
+        url: `${siteUrl}/blog/${post.slug}`,
+        date: post.publishedAt,
+      });
+    });
+    
+    return new Response(feed.xml(), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/xml'
+      }
+    })
+  } catch (error) {
+    console.error('Error generating RSS feed:', error);
+    return new Response('Error generating RSS feed', { status: 500 });
+  }
+}
 }
