bc7ff4478bb802b84b7956875e034daf3daa785c
Lee Robinson <lrobinson2011@gmail.com>
2023-12-09 20:43:38 -0600

    Use SVG sprite. (#680)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { GeistMono } from 'geist/font/mono';
 import { Navbar } from './components/nav';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { PreloadResources } from './preload';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://leerob.io'),
@@ -64,6 +65,7 @@ export default function RootLayout({
           {children}
           <Analytics />
           <SpeedInsights />
+          <PreloadResources />
         </main>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import typography from '@tailwindcss/typography';
 
 export default {
-  content: ['./app/**/*.{ts,tsx}', './content/**/*.mdx'],
+  content: ['./app/**/*.{ts,tsx}', './content/**/*.mdx', './public/**/*.svg'],
   theme: {
     extend: {
       fontFamily: {
