75ddcc673e381aa45d9fab3125e0b2ce123b0a2c
Lee Robinson <lrobinson2011@gmail.com>
2023-12-18 12:34:24 -0600

    Fix broken post.

--- app/components/tweet.tsx
+++ app/components/tweet.tsx
@@ -1,5 +1,11 @@
 import { getTweet } from 'react-tweet/api';
-import { EmbeddedTweet, TweetNotFound, type TweetProps } from 'react-tweet';
+import { Suspense } from 'react';
+import {
+  TweetSkeleton,
+  EmbeddedTweet,
+  TweetNotFound,
+  type TweetProps,
+} from 'react-tweet';
 import './tweet.css';
 
 const TweetContent = async ({ id, components, onError }: TweetProps) => {
@@ -29,7 +35,9 @@ export async function TweetComponent({ id }: { id: string }) {
   return (
     <div className="tweet my-6">
       <div className={`flex justify-center`}>
+        {/* <Suspense fallback={<TweetSkeleton />}> */}
         <ReactTweet id={id} />
+        {/* </Suspense> */}
       </div>
     </div>
   );
