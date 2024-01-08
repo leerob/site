947e21a8b5b9a00d084584b0d44cb322406fccf1
Lee Robinson <lrobinson2011@gmail.com>
2024-01-07 19:19:56 -0600

    Try an error boundary.

--- app/components/sandpack.tsx
+++ app/components/sandpack.tsx
@@ -10,7 +10,33 @@ import {
   stylexViteConfig,
   stylexTokens,
 } from './sandpack-files';
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
+
+class ErrorBoundary extends React.Component<
+  { fallback: React.ReactNode; children?: React.ReactNode },
+  { hasError: boolean }
+> {
+  constructor(props: { fallback: React.ReactNode; children: React.ReactNode }) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="px-4 py-3 border border-red-700 bg-red-200 rounded p-1 text-sm flex items-center text-red-900 mb-8">
+          <div className="w-full callout">{this.props.fallback}</div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
 
 export function LiveCode({ example }: { example: string }) {
   let files;
@@ -30,37 +56,45 @@ export function LiveCode({ example }: { example: string }) {
   } else if (example === 'stylex') {
     return (
       <Suspense fallback={null}>
-        <Sandpack
-          theme="auto"
-          files={{
-            'App.tsx': {
-              code: stylexApp,
-              active: true,
-            },
-            '/tokens.stylex.js': stylexTokens,
-            '/vite.config.ts': stylexViteConfig,
-            '/index.html': {
-              code: stylexIndex,
-              hidden: true,
-            },
-          }}
-          template="vite-react-ts"
-          customSetup={{
-            dependencies: {
-              '@stylexjs/stylex': '^0.3.0',
-            },
-            devDependencies: {
-              'vite-plugin-stylex-dev': 'latest',
-            },
-          }}
-        />
+        <ErrorBoundary
+          fallback={'Oops, there was an error loading the CodeSandbox'}
+        >
+          <Sandpack
+            theme="auto"
+            files={{
+              'App.tsx': {
+                code: stylexApp,
+                active: true,
+              },
+              '/tokens.stylex.js': stylexTokens,
+              '/vite.config.ts': stylexViteConfig,
+              '/index.html': {
+                code: stylexIndex,
+                hidden: true,
+              },
+            }}
+            template="vite-react-ts"
+            customSetup={{
+              dependencies: {
+                '@stylexjs/stylex': '^0.3.0',
+              },
+              devDependencies: {
+                'vite-plugin-stylex-dev': 'latest',
+              },
+            }}
+          />
+        </ErrorBoundary>
       </Suspense>
     );
   }
 
   return (
     <Suspense fallback={null}>
-      <Sandpack theme="auto" template="static" files={files} />
+      <ErrorBoundary
+        fallback={'Oops, there was an error loading the CodeSandbox.'}
+      >
+        <Sandpack theme="auto" template="static" files={files} />
+      </ErrorBoundary>
     </Suspense>
   );
 }
