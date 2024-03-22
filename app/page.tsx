2d754bef5238de2be226ee1a15b06515a4ef4718
Shounak Datta <shounak14datta@gmail.com>
2024-03-22 00:06:14 -0700

    added pic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+import Image from "next/image";
+import me from "public/images/home/me.jpg"
+
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">hey, I'm sap</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">hey, I'm sap &#128075;</h1>
       <p className="prose prose-neutral dark:prose-invert">
         This is my portfolio.
       </p>
+      <div className="my-8 columns-1 gap-4 sm:columns-2">
+        <div className="relative mb-4 h-80">
+          <Image
+            alt="Me at Quebec City"
+            src={me}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
     </section>
   );
 }
