e845b1127c74b3a4bfe2df173ca4e81bff02dbca
Heemank Verma <heemankv@gmail.com>
2024-06-02 02:21:34 +0530

    build

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,12 @@
 import { getBlogPosts } from 'app/db/blog';
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `https://leerob.io/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
+  // let blogs = getBlogPosts().map((post) => ({
+  //   url: `https://leerob.io/blog/${post.slug}`,
+  //   lastModified: post.metadata.publishedAt,
+  // }));
 
-  let routes = ['', '/blog', '/guestbook', '/uses', '/work'].map((route) => ({
+  let routes = ['', '/guestbook', '/uses', '/work'].map((route) => ({
     url: `https://leerob.io${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
