64d4ee63ee4a971e7e636efbfbc3e7168a64e5d2
Heemank Verma <heemankv@gmail.com>
2024-06-02 02:21:50 +0530

    build

---
+++ app/utils/blog/pagew.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import { Suspense } from 'react';
+import ViewCounter from './view-counter';
+import { getViewsCount } from 'app/db/queries';
+import { getBlogPosts } from 'app/db/blog';
+
+export const metadata = {
+  title: 'Blog',
+  description: 'Read my thoughts on software development, design, and more.',
+};
+
+export default function BlogPage() {
+  let allBlogs = getBlogPosts();
+
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        read my blog
+      </h1>
+      {allBlogs
+        .sort((a, b) => {
+          if (
+            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+          ) {
+            return -1;
+          }
+          return 1;
+        })
+        .map((post) => (
+          <Link
+            key={post.slug}
+            className="flex flex-col space-y-1 mb-4"
+            href={`/blog/${post.slug}`}
+          >
+            <div className="w-full flex flex-col">
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                {post.metadata.title}
+              </p>
+              <Suspense fallback={<p className="h-6" />}>
+                <Views slug={post.slug} />
+              </Suspense>
+            </div>
+          </Link>
+        ))}
+    </section>
+  );
+}
+
+async function Views({ slug }: { slug: string }) {
+  let views = await getViewsCount();
+
+  return <ViewCounter allViews={views} slug={slug} />;
+}
