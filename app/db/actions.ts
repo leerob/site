d185d9574306e70d33780b47d4493ec1f0374b87
Lee Robinson <lrobinson2011@gmail.com>
2024-07-29 23:11:32 -0500

    Guestbook inserts

--- app/db/actions.ts
+++ app/db/actions.ts
@@ -37,8 +37,8 @@ export async function saveGuestbookEntry(formData: FormData) {
   let body = entry.slice(0, 500);
 
   await sql`
-    INSERT INTO guestbook (id, email, body, created_by, created_at)
-    VALUES (RANDOM()+5000, ${email}, ${body}, ${created_by}, NOW())
+    INSERT INTO guestbook (email, body, created_by, created_at)
+    VALUES (${email}, ${body}, ${created_by}, NOW())
   `;
 
   revalidatePath('/guestbook');
