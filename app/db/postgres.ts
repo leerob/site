1cd68018398d61d078c73da3368add6df2e9e264
Lee Robinson <lrobinson2011@gmail.com>
2024-09-06 21:06:32 -0500

    New minimal design. (#727)

--- app/db/postgres.ts
+++
@@ -1,5 +0,0 @@
-import postgres from 'postgres';
-
-export const sql = postgres(process.env.POSTGRES_URL!, {
-  ssl: 'allow',
-});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-import './global.css';
-import type { Metadata } from 'next';
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
-import { Navbar } from './components/nav';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from 'next/font/google';
+import { ViewTransitions } from "next-view-transitions";
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { SandpackCSS } from './blog/[slug]/sandpack';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://leerob.io'),
@@ -13,63 +12,54 @@ export const metadata: Metadata = {
     default: 'Lee Robinson',
     template: '%s | Lee Robinson',
   },
-  description: 'Developer, writer, and creator.',
-  openGraph: {
-    title: 'Lee Robinson',
-    description: 'Developer, writer, and creator.',
-    url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  twitter: {
-    title: 'Lee Robinson',
-    card: 'summary_large_image',
-  },
-  verification: {
-    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-    yandex: '14d2e73487fa6c71',
-  },
+  description: "Frontend developer, optimist, community builder.",
 };
-
-const cx = (...classes) => classes.filter(Boolean).join(' ');
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html
-      lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
-      <head>
-        <SandpackCSS />
-      </head>
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Analytics />
-          <SpeedInsights />
-        </main>
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang="en" className={inter.className}>
+        <body className="antialiased tracking-tight">
+          <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-white text-gray-900">
+            <main className="max-w-[60ch] mx-auto w-full space-y-6">
+              {children}
+            </main>
+            <Footer />
+            <Analytics />
+          </div>
+        </body>
+      </html>
+    </ViewTransitions>
   );
+}
+
+function Footer() {
+  const links = [
+    { name: '@leerob', url: 'https://x.com/leeerob' },
+    { name: 'youtube', url: 'https://www.youtube.com/@leerob' },
+    { name: 'linkedin', url: 'https://www.linkedin.com/in/leeerob' },
+    { name: 'github', url: 'https://github.com/leerob' },
+  ]
+
+  return (
+    <footer className="mt-12 text-center">
+      <div className="flex justify-center space-x-4 tracking-tight">
+        {links.map((link) => (
+          <a
+            key={link.name}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 hover:text-blue-500 transition-colors duration-200"
+          >
+            {link.name}
+          </a>
+        ))}
+      </div>
+    </footer>
+  )
 }
