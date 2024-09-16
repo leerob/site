89cff02fb884e952b995b533ab8b6f4b058f1f57
Lee Robinson <me@leerob.io>
2024-09-16 12:05:18 -0500

    Sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+async function getNoteSlugs(dir: string) {
+  const entries = await fs.readdir(dir, {
+    recursive: true,
+    withFileTypes: true,
+  });
+  return entries
+    .filter((entry) => entry.isFile() && entry.name === 'page.mdx')
+    .map((entry) => {
+      const relativePath = path.relative(
+        dir,
+        path.join(entry.parentPath, entry.name)
+      );
+      return path.dirname(relativePath);
+    })
+    .map((slug) => slug.replace(/\\/g, '/'));
+}
+
+export default async function sitemap() {
+  const notesDirectory = path.join(process.cwd(), 'app', 'n');
+  const slugs = await getNoteSlugs(notesDirectory);
+
+  const notes = slugs.map((slug) => ({
+    url: `https://leerob.io/n/${slug}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  const routes = ['', '/work'].map((route) => ({
+    url: `https://leerob.io${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...routes, ...notes];
+}
