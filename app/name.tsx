e3a62ed58e9d399a6d97134de7fdb90b62ff3752
Lee Robinson <me@leerob.io>
2024-10-11 13:59:56 -0500

    Update homepage

---
+++ app/name.tsx
@@ -0,0 +1,31 @@
+export function NameTransition() {
+  return (
+    <h1 className="font-medium pt-12 transition-element">
+      <span className="sr-only">Lee Robinson</span>
+      <span aria-hidden="true" className="block overflow-hidden group relative">
+        <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full">
+          {'Lee Robinson'.split('').map((letter, index) => (
+            <span
+              key={index}
+              className="inline-block"
+              style={{ transitionDelay: `${index * 25}ms` }}
+            >
+              {letter === ' ' ? '\u00A0' : letter}
+            </span>
+          ))}
+        </span>
+        <span className="inline-block absolute left-0 top-0 transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0">
+          {'leerob'.split('').map((letter, index) => (
+            <span
+              key={index}
+              className="inline-block"
+              style={{ transitionDelay: `${index * 25}ms` }}
+            >
+              {letter}
+            </span>
+          ))}
+        </span>
+      </span>
+    </h1>
+  );
+}
