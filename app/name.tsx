ff98250df7e3f9beb60a646b0e2e55d16b81a49a
Leo Giovanetti <hello@leog.me>
2025-01-06 22:53:13 -0300

    Fixing #735

--- app/name.tsx
+++ app/name.tsx
@@ -3,7 +3,7 @@ export function NameTransition() {
     <h1 className="font-medium pt-12 transition-element">
       <span className="sr-only">Lee Robinson</span>
       <span aria-hidden="true" className="block overflow-hidden group relative">
-        <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full">
+        <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full whitespace-nowrap">
           {'Lee Robinson'.split('').map((letter, index) => (
             <span
               key={index}
