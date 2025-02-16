3fcc9cd9bbe9eafa388d6118f782b410bb13b37d
ku <2959002395@qq.com>
2025-02-16 15:26:21 +0800

    Delete unused Link declaration

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,4 @@
 import React, { ComponentPropsWithoutRef } from 'react';
-import Link from 'next/link';
 import { highlight } from 'sugar-high';
 
 type HeadingProps = ComponentPropsWithoutRef<'h1'>;
