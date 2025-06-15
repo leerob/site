4d81d0b2cf850a94e39084d569d766f7e50af0d9
Shounak Datta <shounak14datta@gmail.com>
2025-06-15 11:09:10 -0700

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">hey, I'm sap &#128075;</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is my portfolio.
+        I'm an engineer turned project manager who’s spent the last few years helping build everything from airplanes to climate tech. These days, I’m at Heirloom working on direct air carbon capture (literally pulling CO₂ out of the sky to help fight climate change!)
       </p>
       <div className="my-8 columns-1 gap-4 sm:columns-2">
         <div className="relative mb-4 h-80">
@@ -19,7 +19,19 @@ export default function Page() {
             className="rounded-lg object-cover"
           />
         </div>
+        <div className="relative mb-4 h-80">
+          <p className="prose prose-neutral dark:prose-invert">
+            I’ve always loved solving tough problems, working with smart people, and learning new things along the way. Whether it’s managing complex engineering projects or figuring out how to make something work in the real world, I like being in the thick of it.
+            <br></br>
+            <br></br>
+            Outside of work, you’ll usually find me reading about systems thinking, tinkering with side projects, or hunting for the best coffee in town.
+          </p>
+        </div>
       </div>
+      <p className="prose prose-neutral dark:prose-invert">
+        Since you're here, why not check out what I've been working? Pop over to the work tab to see my latest projects,
+        or the blog tab to hear me rant about what's in the news!
+      </p>
     </section>
   );
 }
